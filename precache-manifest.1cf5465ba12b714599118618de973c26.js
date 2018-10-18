e2a1bf8d2c4c7162e8ce041c1ffb02444d582056
vd1992 <40485260+vd1992@users.noreply.github.com>
2018-10-17 23:06:07 -0600

    Adding build version of first react app

---
+++ precache-manifest.1cf5465ba12b714599118618de973c26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e4bf96ca52d211a6833",
+    "url": "/static/js/main.5e4bf96c.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "5e4bf96ca52d211a6833",
+    "url": "/static/css/main.7dde19c3.chunk.css"
+  },
+  {
+    "revision": "78165fc097748272d9d56abaea3d8d07",
+    "url": "/index.html"
+  }
+];
