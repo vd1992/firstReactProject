10abb9d45e2bef459e65c7ec7a6371fcb9dc46f9
vd1992 <40485260+vd1992@users.noreply.github.com>
2018-11-12 18:17:15 -0700

    a

---
+++ my_app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>My First React App, thank you https://github.com/facebook/create-react-app</p>
+          <p>pardon my dust</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
